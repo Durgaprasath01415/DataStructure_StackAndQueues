69a297ae0a3e337d7110ad84bdc71f34ad25769a
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-06-10 15:40:24 +0530

    ADDED : Classes for preforming stack operation

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -1,11 +1,3 @@
-public class Stack<E> {
-    LinkedList<Integer> myLinkedList = new LinkedList<>();
+public class Stack{
 
-    public void push(INode newNode) {
-        myLinkedList.add(newNode);
-    }
-
-    public INode peek() {
-        return myLinkedList.head;
-    }
 }
