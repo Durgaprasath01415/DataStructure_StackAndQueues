ef9491d122931ba9746be44dbc9f08af0e55ba46
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-06-10 15:44:51 +0530

    ADDED : Functionlity to add to stack and peek into stack

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -1,3 +1,11 @@
-public class Stack{
+public class Stack<E> {
+    LinkedList<Integer> myLinkedList = new LinkedList<>();
 
+    public void push(INode newNode) {
+        myLinkedList.add(newNode);
+    }
+
+    public INode peek() {
+        return myLinkedList.head;
+    }
 }
--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,3 +1,18 @@
-public class StackTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class StackTest {
+    @Test
+    public void ifStackIsCreated_shouldReturnTrue() {
+        Stack<Integer> stack = new Stack<>();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        stack.push(myThirdNode);
+        stack.push(mySecondNode);
+        stack.push(myFirstNode);
+        INode peek = stack.peek();
+        Assert.assertEquals(peek, myFirstNode);
+    }
 }
+
